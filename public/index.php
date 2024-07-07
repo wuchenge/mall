e567871cc704683bd79d23c3e9cd63684f845c55
李圭 <ligui0506@sina.com>
2024-07-07 20:59:14 +0800

    Update index.php
    test git pull

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+echo 11;die;
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
